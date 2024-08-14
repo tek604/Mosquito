40772febc6b4e28a2044624c1ff4dc4ac42e1201
tek_604 <tek.604@gmail.com>
2024-08-14 11:58:41 +0200

    Added first version of config files

---
+++ Config/joystick/RIGHT VPC Stick WarBRD-D {2D824510-3C5A-11ef-8001-444553540000}.diff.lua
@@ -0,0 +1,31 @@
+local diff = {
+	["axisDiffs"] = {
+		["a3028cd3"] = {
+			["name"] = "Throttle, engine (selected) - axis",
+			["removed"] = {
+				[1] = {
+					["key"] = "JOY_Z",
+				},
+			},
+		},
+	},
+	["keyDiffs"] = {
+		["d1591pnilu1592cdnilvdnilvpnilvunil"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "JOY_BTN5",
+				},
+			},
+			["name"] = "COMM Push to talk (call radio menu)",
+		},
+		["dnilp74u75cdnilvdnilvpnilvunil"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "JOY_BTN31",
+				},
+			},
+			["name"] = "Wheel Brakes",
+		},
+	},
+}
+return diff
